509d24171316134c47339d7e8ccdbbd68c36de8f
Sam Corin <samcorin@gmail.com>
2016-01-24 22:50:31 +0900

    minor tweaks

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var board = ["tl", "tc", "tr", "ml", "mc", "mr", "bl", "bc", "br"];
 var combinations = [
     ["tl", "tc", "tr"],
@@ -12,7 +11,7 @@ var combinations = [
 ];
 var board_copy = board.slice();
 var can_move = true;
-
+var player_choose = true;
 var Player = function(name, id) {
     this.name = name;
     this.id = id;
@@ -28,7 +27,7 @@ Player.prototype.reset = function() {
 var player = new Player('player', 'X');
 var computer = new Player('computer', '0');
 
-$(document).ready(function() {    
+$(document).ready(function() {
     $(".options").click(function(event) {
         player.id = event.target.id;
         if (player.id == '0') {
@@ -38,29 +37,36 @@ $(document).ready(function() {
             player.id = 'X';
             computer.id = '0';
         }
+        // Random start
+        if (Math.random() > 0.5) {
+            computer_move();
+        }
     });
 
     // Random start
-    if(Math.random() > 0.5) {
-        computer_move();
+    if (player_choose) {
+        player_choose = false;
+        setTimeout(function() {
+            $('#restart').trigger('click').removeAttr("data-toggle");
+        }, 1000);
     }
 
     function check_for_move(who) {
         combinations.forEach(function(val, i) {
             var count = 0;
             for (var i = 0; i < who.moves.length; i++) {
-                if(val.indexOf(who.moves[i]) != -1) {
+                if (val.indexOf(who.moves[i]) != -1) {
                     count++;
                 };
-                if(count == 3) {
+                if (count == 3) {
                     can_move = false;
                     win(val, who);
                 };
-                if(count == 2) {  
-                    var make_move = val.filter(function(v,i) {
+                if (count == 2) {
+                    var make_move = val.filter(function(v, i) {
                         return who.moves.indexOf(v) < 0;
                     });
-                    if(make_move && can_move && $('#'+make_move).is(":empty")) {
+                    if (make_move && can_move && $('#' + make_move).is(":empty")) {
                         can_move = false;
                         add_to_html(make_move[0], computer);
                     }
@@ -71,9 +77,9 @@ $(document).ready(function() {
 
     function win(val, who) {
         who.score++;
-        console.log(who.id + " WINS!");
-        $("td").prop("disabled", true);
-        
+
+        $("td").prop( "disabled", true );
+
         val.forEach(function(a) {
             $('#' + a).addClass('winner');
         })
@@ -85,30 +91,30 @@ $(document).ready(function() {
     function restart() {
         console.log('GAME OVER');
         $("td").prop("disabled", true);
-        
+
         setTimeout(function() {
             $("#restart").trigger('click');
         }, 400);
     }
 
     function add_to_html(id, who) {
-        $('#'+id).html("<p>" + who.id + "</p>");
+        $('#' + id).html("<p>" + who.id + "</p>");
         board_copy.splice(board_copy.indexOf(id), 1);
         who.moves.push(id);
     }
 
     function computer_move() {
         can_move = true;
-        if(computer.moves.length >= 2) {
+        if (computer.moves.length >= 2) {
             check_for_move(computer);
         }
 
-        if(player.moves.length >= 2 && can_move) {
+        if (player.moves.length >= 2 && can_move) {
             check_for_move(player);
         };
 
-        if(can_move) {
-            var randomMove = board_copy[Math.floor(Math.random()* board_copy.length)];
+        if (can_move) {
+            var randomMove = board_copy[Math.floor(Math.random() * board_copy.length)];
             add_to_html(randomMove, computer);
         }
     }
@@ -121,9 +127,9 @@ $(document).ready(function() {
             add_to_html(target.id, player);
             setTimeout(function() {
                 computer_move();
-            }, time+=150)
-            
-            if(board_copy.length == 0) {
+            }, time += 100)
+
+            if (board_copy.length == 0) {
                 setTimeout(function() {
                     restart();
                 }, 300)
@@ -132,14 +138,14 @@ $(document).ready(function() {
     })
 
     // RESET GAME
-    $("#restart").click(function() {
+    $("#restart").click(function(e) {
         $('.player_score').text(player.score);
         $('.computer_score').text(computer.score);
         board_copy = board.slice();
         can_move = true;
         player.reset();
         computer.reset();
-        $("td").prop("disabled", false);
+        $("td").prop( "disabled", false );
         $("td").each(function() {
             $(this).removeClass('winner').text("");
         });
